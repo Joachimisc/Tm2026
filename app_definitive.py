932f64bc9894ed942da6050f2ccf2aa71103ccfb
Joachimisc <joachim.ischi@eduvaud.ch>
2026-07-15 18:01:34 +0200

    premier essais pour faire apparaitre le site ( sortie de la copie des exemples de la prof)

---
+++ app_definitive.py
@@ -0,0 +1,21 @@
+from nicegui import ui
+import classes
+import tuiles
+
+plateau = classes.Plateau()
+
+partie = None
+
+@ui.page('/')
+def accueil():
+    nom1 = ui.input("Joueur 1")
+    nom2 = ui.input("Joueur 2")
+
+    def commencer():
+        global partie
+        joueurs = [classes.Joueur(nom1.value),classes.Joueur(nom2.value)]
+        partie = classes.Partie(joueurs, tuiles.pioche)
+        ui.navigate.to('/jeu')
+
+    ui.button("Commencer", on_click=commencer)
+ui.run()
